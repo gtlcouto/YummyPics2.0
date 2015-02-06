96bedb957da96f7b47403373305db1212eee2442
diegocichello <diegocichello@Diegos-MacBook-Pro.local>
2015-02-05 21:58:32 -0800

    Today changes hope it dont bug

---
+++ Ymmy Pics/CommentsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CommentsViewController.h
+//  Ymmy Pics
+//
+//  Created by Diego Cichello on 2/5/15.
+//  Copyright (c) 2015 Gustavo Couto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommentsViewController : UIViewController
+
+@end
---
+++ Ymmy Pics/CommentsViewController.m
@@ -0,0 +1,13 @@
+//
+//  CommentsViewController.m
+//  Ymmy Pics
+//
+//  Created by Diego Cichello on 2/5/15.
+//  Copyright (c) 2015 Gustavo Couto. All rights reserved.
+//
+
+#import "CommentsViewController.h"
+
+@implementation CommentsViewController
+
+@end
